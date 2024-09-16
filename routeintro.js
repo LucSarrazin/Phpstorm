fcb1e71fc39b5bbb4ad59be0b94c7d1bb581a4ac
luc.sarrazin <luc.sarrazin@free.fr>
2024-09-16 12:17:41 +0200

    suite cours 16/09/24

--- routeintro.js
+++ routeintro.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const app = express()
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const express = require('express');
+const app = express();
 const path = require('path');
+
+import { listeEleves } from "./sources/eleves.mjs";
+
 
 app.get('/', (requete,reponse) =>{
     reponse.send('1ere route express en SIO2')
@@ -16,6 +22,33 @@ app.get('/bienvenue', (requete,reponse) =>{
 
 app.get('/panorama', (requete,reponse) =>{
     reponse.sendFile(path.join(__dirname + '/planDuSite.html'));
+})
+
+// les deux façons pour les paramètres
+// Façon 1
+app.get('/etudiant', (requete,reponse) =>{
+    const nomEtudiant = requete.query.nomEtudiant;
+    reponse.send('Bonjour mr ou mme ' + nomEtudiant)
+})
+// Façon 2
+app.get('/etudiant2/:idEleve', (requete,reponse) =>{
+    const nomEtudiant = requete.params.idEleve;
+    reponse.send('Bonjour mr ou mme ' + nomEtudiant)
+})
+
+app.get('/nombre', (requete,reponse) =>{
+    const parametre = requete.query.nombre
+    const Nombre = Number(parametre);
+    if(Number.isInteger(Nombre)){
+        reponse.send('Le nombre choisi est ' + Nombre)
+    }
+    else{
+        reponse.send(parametre + " Le nombre choisi n'est pas un nombre ")
+    }
+})
+
+app.get('/api/leseleves', (requete,reponse) =>{
+    reponse.json(listeEleves)
 })
 
 app.listen(3000)
---
+++ sources/eleves.mjs
@@ -0,0 +1,6 @@
+const listeEleves = [
+    { nom: 'Beratto', prenom: 'Elias', age: 18},
+    { nom: 'Gougeon', prenom: 'Sterenn', age: 19},
+    { nom: 'Sarrazin', prenom: 'Luc', age: 19}
+]
+export {listeEleves}
